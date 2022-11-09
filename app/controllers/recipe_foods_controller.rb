76c350026b759e23365654039a67738b5e367dd5
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-09 14:35:06 +0100

    Add recipe_foods file, and controllers and model for recipe_foods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,13 +3,13 @@ class RecipesFoodController < ApplicationController
     @user = current_user
     @recipe = Recipe.find(params[:recipe_id])
     @food = @user.foods
-    @recipes_food = RecipesFood.new
+    @recipe_foods = RecipeFoods.new
   end
 
   def create
     @recipe = Recipe.find(params[:recipe_id])
-    @recipes_food = RecipesFood.new(recipes_food_params.merge(recipe_id: @recipe.id))
-    if @recipes_food.save
+    @recipe_foods = RecipeFoods.new(recipe_foods_params.merge(recipe_id: @recipe.id))
+    if @recipe_foods.save
       redirect_to recipe_path(@recipe)
     else
       render :new
@@ -18,8 +18,8 @@ class RecipesFoodController < ApplicationController
 
   def destroy
     @recipe = Recipe.find(params[:recipe_id])
-    @recipes_food = RecipesFood.find(params[:id])
-    @recipes_food.destroy
+    @recipe_foods = RecipeFoods.find(params[:id])
+    @recipe_foods.destroy
     respond_to do |format|
       format.html { redirect_to recipe_path(@recipe), notice: 'Food was successfully removed.' }
       format.json { head :no_content }
@@ -28,7 +28,7 @@ class RecipesFoodController < ApplicationController
 
   private
 
-  def recipes_food_params
+  def recipe_foods_params
     params.require(:recipes_food).permit(:food_id)
   end
 
